84bc1228db711fac1d2ea0b0fec25feefdf40f17
greenfil <mikl@greenfil.ru>
2017-04-01 15:30:40 +0300

    Добавил методы задания/чтения языка ввод/вывода. Вывод списка доступных языков

--- app/src/main/java/ru/greenfil/translator/ITranslator.java
+++ app/src/main/java/ru/greenfil/translator/ITranslator.java
@@ -1,10 +1,25 @@
 package ru.greenfil.translator;
+import android.widget.TextView;
 
+import java.util.List;
+
+import ru.greenfil.translator.ILanguage;
 /**
  * Created by greenfil on 01.04.17.
  */
 
 
 public interface ITranslator {
+    void SetSourceLang(ILanguage _Lang);
+    ILanguage GetSourceLang();
+    void SetTargetLang(ILanguage _Lang);
+    ILanguage GetTargetLang();
+    void SwapLang();
+    void SetText(String _Text);
 
+    //Поменять на что-то более вменяемое
+    void SetTextOut(TextView _TextOut);
+
+    List<ILanguage> GetLanguageList();
+    //Позже можно подумать о подгрузке списка языков
 }
