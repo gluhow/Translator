dca24f844eb818973b3179249eeacd58319b1750
greenfil <mikl@greenfil.ru>
2017-04-01 22:01:54 +0300

    Добавил свойства Caption и UI

--- app/src/main/java/ru/greenfil/translator/ILanguage.java
+++ app/src/main/java/ru/greenfil/translator/ILanguage.java
@@ -4,5 +4,7 @@ package ru.greenfil.translator;
  * Created by greenfil on 01.04.17.
  */
 
-public interface ILanguage {
+interface ILanguage {
+    public String GetCaption();
+    public String GetUI();
 }
