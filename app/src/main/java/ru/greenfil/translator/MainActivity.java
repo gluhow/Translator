24cf0b8cd31b75d11fc1e5327f953ed24c03e51e
greenfil <mikl@greenfil.ru>
2017-04-05 21:27:56 +0300

    Первая попытка послать вылезти в инет. Всё падает, надо пробоваить через поток.

--- app/src/main/java/ru/greenfil/translator/MainActivity.java
+++ app/src/main/java/ru/greenfil/translator/MainActivity.java
@@ -4,50 +4,72 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.MultiAutoCompleteTextView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView textOut;
-    ITranslator curTranslator;
-    MultiAutoCompleteTextView inputText;
+    ITranslator mytranslator;
+    MultiAutoCompleteTextView textIn;
     Spinner SourceSpinner;
     ArrayAdapter<ILanguage> sourceAdapter;
     Spinner TargetSpinner;
     ArrayAdapter<ILanguage> targetAdapter;
+    ArrayList<ILanguage> languageList;
+    Button translateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textOut=(TextView)findViewById(R.id.textView);
+        textOut=(TextView)findViewById(R.id.outputText);
         textOut.setText("");
-        inputText=(MultiAutoCompleteTextView)findViewById(R.id.InputText);
-        inputText.setText("");
-        curTranslator = new YaTranslator();
+        textIn=(MultiAutoCompleteTextView)findViewById(R.id.InputText);
+        textIn.setText("");
+        languageList=new ArrayList<ILanguage>();
+        languageList.add(new TLanguage("English","en"));
+        languageList.add(new TLanguage("Русский","ru"));
+        mytranslator = new YaTranslator();
         SourceSpinner=(Spinner)findViewById(R.id.SourceSpinner);
         sourceAdapter = new ArrayAdapter<ILanguage>(this, android.R.layout.simple_spinner_item,
-                curTranslator.GetSourceLanguageList());
+                languageList);
         sourceAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
         SourceSpinner.setAdapter(sourceAdapter);
 
         TargetSpinner=(Spinner)findViewById(R.id.TargetSpinner);
         targetAdapter = new ArrayAdapter<ILanguage>(this, android.R.layout.simple_spinner_item,
-                curTranslator.GetTargetLanguageList());
+                languageList);
         targetAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
         TargetSpinner.setAdapter(targetAdapter);
+
+        translateButton=(Button)findViewById(R.id.translateButton);
+        translateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    textOut.setText(mytranslator.Translate(textIn.getText().toString(),
+                            languageList.get(0), languageList.get(1)));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
         FormUpdate();
     }
 
     protected void FormUpdate(){
-        SourceSpinner.setSelection(sourceAdapter.getPosition(curTranslator.GetSourceLang()));
-        TargetSpinner.setSelection(targetAdapter.getPosition(curTranslator.GetTargetLang()));
+        /*SourceSpinner.setSelection(sourceAdapter.getPosition(curTranslator.GetSourceLang()));
+        TargetSpinner.setSelection(targetAdapter.getPosition(curTranslator.GetTargetLang()));*/
     }
 
     public void OnSwapLanguage(View view) {
-        curTranslator.SwapLang();
         FormUpdate();
     }
 }
