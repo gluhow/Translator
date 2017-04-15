bac4c276a10d2f80e1f999df393a9ed212420b25
greenfil <mikl@greenfil.ru>
2017-04-15 16:45:57 +0300

    Реализовал Основные свойства язык1-2 и входной текст. Доп. - результат

--- app/src/main/java/ru/greenfil/translator/TOneWord.java
+++ app/src/main/java/ru/greenfil/translator/TOneWord.java
@@ -1,10 +1,61 @@
 package ru.greenfil.translator;
 
-import ru.greenfil.translator.ILanguage;
-
 /**
- * В классе зранится одно переведенное словоы
+ * В классе зранится одно переведенное слово
  */
 
-public class TOneWord {
+class TOneWord {
+    private ILanguage sourceLang;
+    private ILanguage targetLang;
+    private String sourceText;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TOneWord tOneWord = (TOneWord) o;
+
+        return sourceLang.equals(tOneWord.sourceLang) &&
+                targetLang.equals(tOneWord.targetLang) &&
+                sourceText.equals(tOneWord.sourceText);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = sourceLang.hashCode();
+        result = 31 * result + targetLang.hashCode();
+        result = 31 * result + sourceText.hashCode();
+        return result;
+    }
+
+    void setTargetText(String targetText) {
+        this.targetText = targetText;
+    }
+
+    private String targetText;
+
+    ILanguage getSourceLang() {
+        return sourceLang;
+    }
+
+    ILanguage getTargetLang() {
+        return targetLang;
+    }
+
+    String getSourceText() {
+        return sourceText;
+    }
+
+    String getTargetText() {
+        return targetText;
+    }
+
+    TOneWord(ILanguage sourceLang, ILanguage targetLang, String sourceText) {
+        this.sourceLang = sourceLang;
+        this.targetLang = targetLang;
+        this.sourceText = sourceText;
+        this.targetText = "";
+    }
 }
