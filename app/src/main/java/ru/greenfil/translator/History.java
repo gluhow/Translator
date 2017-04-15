8b5468b334c15bedc77cfcf362ba3e3c510e6066
greenfil <mikl@greenfil.ru>
2017-04-15 21:32:02 +0300

    Класс страницы "История"

---
+++ app/src/main/java/ru/greenfil/translator/History.java
@@ -0,0 +1,45 @@
+package ru.greenfil.translator;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class History extends AppCompatActivity {
+
+    //private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            /*switch (item.getItemId()) {
+                case R.id.action_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.action_history:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.action_favorites:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }*/
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+
+/*        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);*/
+    }
+
+}
