c5c2f1c11be35da457c1310b32acb5bcc63ccb90
greenfil <mikl@greenfil.ru>
2017-04-23 14:46:53 +0300

    Убра предупреждения

--- app/src/main/java/ru/greenfil/translator/YaTranslator.java
+++ app/src/main/java/ru/greenfil/translator/YaTranslator.java
@@ -17,27 +17,24 @@ import javax.net.ssl.HttpsURLConnection;
 
 class YaTranslator implements ITranslator {
     private final Integer some_error=1;
-    private String key="trnsl.1.1.20170408T105508Z.9e4323ceef72527b.6578d71fe0273638aa0c3cd75b9d5a334115404f";
     private Integer fErrRes=some_error;
 
-    void SetKey(String key){
-        this.key=key;
-    }
-
+    /**
+     * Получить ответ от сервера Яндекса
+     * text - исходный текст
+     * sourceLang - исходный язык
+     * targetLang - целевой язык
+     */
     private String GetWebAnswer(String text, String sourceLang, String targetLang) {
-        /**
-         * Получить ответ от сервера Яндекса
-         * text - исходный текст
-         * sourceLang - исходный язык
-         * targetLang - целевой язык
-         */
+        final String
+                APIkey="trnsl.1.1.20170408T105508Z.9e4323ceef72527b.6578d71fe0273638aa0c3cd75b9d5a334115404f";
         fErrRes=some_error;
         String yaURL="https://translate.yandex.net/api/v1.5/tr.json/translate ?";
         String params="";
         try {
             params = String.format(
                     "key=%s&text=%s&lang=%s-%s",
-                    key,
+                    APIkey,
                     URLEncoder.encode(text, "UTF-8"),
                     sourceLang,
                     targetLang);
@@ -80,10 +77,11 @@ class YaTranslator implements ITranslator {
 
         return res;
     }
+
+    /**
+     * Получить текст перевода по JSON-строке
+     */
     private String GetTextFROMJSON(String text){
-        /**
-         * Получить текст перевода по JSON-строке
-         */
         try {
             JSONObject resJSON = new JSONObject(text);
             JSONArray textAr = resJSON.getJSONArray("text");
@@ -100,11 +98,11 @@ class YaTranslator implements ITranslator {
         return text;
     }
 
+    /**
+     * Перевести текст
+     */
     @Override
     public String Translate(String MyText, ILanguage SourceLanguage, ILanguage TargetLanguage) {
-        /**
-         * Перевести текст
-         */
         String WebAns = GetWebAnswer(MyText, SourceLanguage.GetUI(), TargetLanguage.GetUI());
         return GetTextFROMJSON(WebAns);
         //Перенести формирование кодов ошибки сюда!
