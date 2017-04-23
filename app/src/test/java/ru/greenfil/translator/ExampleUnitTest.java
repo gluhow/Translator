50abb99e62345f98fe39f796e59393cccc3ccbd1
greenfil <mikl@greenfil.ru>
2017-04-23 15:32:23 +0300

    Вернул юнит тест на место, т.к. не знаю как замокать объекты

--- app/src/test/java/ru/greenfil/translator/ExampleUnitTest.java
+++ app/src/test/java/ru/greenfil/translator/ExampleUnitTest.java
@@ -1,59 +1,17 @@
 package ru.greenfil.translator;
 
-import android.os.Bundle;
-
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 /**
- * Попытка написать хоть какие-то юнит-тесты
+ * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-
-class TranslatorUnitTest implements ITranslator{
-
-    @Override
-    public String Translate(String MyText, ILanguage SourceLanguage, ILanguage TargetLanguage) {
-        return null;
-    }
-
-    @Override
-    public int ErrCode() {
-        return 0;
-    }
-}
-
-class ActivityUnitTest extends MainActivity {
-    @Override
-    void LoadData() {
-
-    }
-
-    @Override
-    protected ITranslator GetTranslator() {
-        return super.GetTranslator();//  new TranslatorUnitTest();
-    }
-}
-
 public class ExampleUnitTest {
-    private MainActivity activity;
-    @Before
-    public void SetUp(){
-       activity=new MainActivity();
-    }
-
     @Test
-    public void activityNotNull(){
-        assertNotNull(activity);
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
     }
-
-    @Test
-    public void translatorNotNull(){
-        assertNotNull(activity.mytranslator);
-    }
-
-
 }
